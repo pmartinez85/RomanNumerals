335a596f192c7f32869cebd7553a057ff77a4c1c
Pedro Martinez <pmartinez1085@gmail.com>
2016-10-25 16:23:58 +0200

    array v2

--- src/RomanNumber.php
+++ src/RomanNumber.php
@@ -2,22 +2,31 @@
 
 class RomanNumbers{
     protected $code = [
-        1 => "I",
-        4 => "IV",
-        5 => "V",
+        1000=>"M",
+        900=>"CM",
+        500=>"D",
+        400=>"CD",
+        100 =>"C",
+        90 => "XC",
+        50 => "L",
+        40 => "XL",
+        10 => "X",
         9 => "IX",
-        10 => "X"
-        ];
+        5 => "V",
+        4 => "IV",
+        1 => "I"
+    ];
 
     public function convert($number){
-        $solution = "I";
+        $solution = "";
+       foreach ($this->code as $decimal => $roman){
 
-        if ($number>=4) {
-            $solution = "IV";
-            $number=0;
-        }
+           if ($number >= $decimal){
+               $solution .= $roman;
+               $number -= $decimal;
+           }
 
-        $solution =$solution . str_repeat('I', $number);
+       }
         return $solution;
     }
 }
